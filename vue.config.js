07ea16f4ebb595f831a1ea43cf1083b2b8df7ee4
osbert <osbert@implustech.com>
2018-03-26 10:17:36 +0800

    limit webpack to only mind frontend's business

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   lintOnSave: false,
   configureWebpack: {
+    entry: {
+      app : __dirname + "/src/frontend/main.ts"
+    },
     output: {
       path: __dirname + "/dist/frontend"
     }

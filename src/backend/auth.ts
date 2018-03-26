e3f380953969a297a571e168c400bc40b288cdb7
osbert <osbert@implustech.com>
2018-03-26 20:32:03 +0800

    add dummy authentication

---
+++ src/backend/auth.ts
@@ -0,0 +1,27 @@
+import * as KoaPassport from 'koa-passport';
+import * as passport from 'passport';
+import * as express from 'express';
+
+class DumbStrategy implements passport.Strategy {
+
+  public name: string;
+
+  constructor() {
+    this.name = 'dump_strategy';
+  }
+
+  public authenticate(req: express.Request, options?: any): any {
+    return undefined;
+  }
+
+}
+
+KoaPassport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+KoaPassport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+KoaPassport.use(new DumbStrategy());

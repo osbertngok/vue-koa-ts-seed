65b52f0c5e822d83705edb6c5d8029120f9c449f
osbert <osbert@implustech.com>
2018-03-28 21:22:30 +0800

    importing full elementui

--- src/frontend/views/Login.vue
+++ src/frontend/views/Login.vue
@@ -1,5 +1,50 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <el-form ref="form" :model="form" label-width="80px">
+      <el-form-item label="Username">
+        <el-input v-model="form.username"></el-input>
+      </el-form-item>
+      <el-form-item label="Password">
+        <el-input v-model="form.password"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">Login</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
+
+<script>
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+export default {
+  data() {
+    return {
+      form: {
+        username: '',
+        passwords: '',
+      },
+    };
+  },
+  methods: {
+    onSubmit() {
+      console.log('submit!');
+    },
+  },
+};
+</script>

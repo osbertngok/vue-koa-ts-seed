b924a70b542d09aa270d1240b30d6f90c82064b8
osbert <osbert@implustech.com>
2018-03-28 21:34:07 +0800

    justify center for login form

--- src/frontend/views/Login.vue
+++ src/frontend/views/Login.vue
@@ -1,36 +1,24 @@
 <template>
   <div class="about">
-    <el-form ref="form" :model="form" label-width="80px">
-      <el-form-item label="Username">
-        <el-input v-model="form.username"></el-input>
-      </el-form-item>
-      <el-form-item label="Password">
-        <el-input v-model="form.password"></el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="onSubmit">Login</el-button>
-      </el-form-item>
-    </el-form>
+    <el-row :gutter="10" type="flex" justify="center">
+      <el-col :sm="20" :md="8" :lg="8">
+        <el-form ref="form" :model="form" label-width="80px">
+          <el-form-item label="Username">
+            <el-input v-model="form.username"></el-input>
+          </el-form-item>
+          <el-form-item label="Password">
+            <el-input v-model="form.password"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="onSubmit">Login</el-button>
+          </el-form-item>
+        </el-form>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
 
 export default {
   data() {
